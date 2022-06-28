ed03fd8d1909b3c6f5d783126f17da45c2cf0d57
Jonathan Tellez <jonathanbtellez@gmail.com>
2022-06-28 17:01:40 -0500

    Ajustando detalles formulario

---
+++ js/formulario.js
@@ -0,0 +1,45 @@
+var formulario = document.querySelector("#formulario__contacto");
+var nombre = formulario.nombre;
+var email = formulario.email;
+var asunto = formulario.asunto;
+var mensaje = formulario.mensaje;
+var error = document.querySelector("#formulario__error");
+var btnEnviar = document.querySelector("#enviar");
+var reg = new RegExp("^[a-zA-ZÀ-ÿ\s]{1,40}$","g");
+var correoReg = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$","g");
+
+btnEnviar = addEventListener("submit", validarForm)
+
+function validarNombre(event){
+    if(!reg.test(nombre.value)){
+        error.style.display = 'block';
+        error.innerHTML += '<li>El campo nombre no debe contener numeros o simbolos.</li>';
+        event.preventDefault();
+    }else{
+        error.style.display = 'none';
+    }
+}
+function validarCorreo(event){
+    if(!correoReg.test(email.value)){
+        error.style.display = 'block';
+        error.innerHTML += '<li>Verifique que la direccion del correo electronico debe tener una terminacion similar a .com .co .edu etc, no acepta caracteres diferentes a .-_ o numeros.</li>';
+        event.preventDefault();
+    }else{error.style.display = 'none';
+    }
+}
+function validarAsunto(event){
+    if(asunto.value == "" || asunto.value.length < 3){
+        error.style.display = 'block';
+        error.innerHTML += '<li>Por favor introduzca un asunto, Debe contener mas de tres caracteres.</li>';
+        event.preventDefault();
+    }else{error.style.display = 'none';
+    }
+}
+function validarForm(e){
+    // Reiniciamos el error para que inicie sin mensaje.
+    error.innerHTML = '';
+    validarNombre(e);
+    validarCorreo(e);
+    validarAsunto(e);
+}
+
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
-const inicio = document.querySelector("#inicio");
-const menu = document.querySelector(".header__menu");
-const openMenuBtn = document.querySelector(".btn__menu__abrir");
-const closeMenuBtn = document.querySelector(".btn__menu__cerrar");
+var inicio = document.querySelector("#inicio");
+var menu = document.querySelector(".header__menu");
+var openMenuBtn = document.querySelector(".btn__menu__abrir");
+var closeMenuBtn = document.querySelector(".btn__menu__cerrar");
+var menuEnlaces = document.querySelector(".header__menu");//clase padre
+var enlaceMenu = menuEnlaces.querySelectorAll(".header__menu_item")
 function toggleMenu(){
     menu.classList.toggle("invisible");
 }
@@ -9,4 +11,7 @@ openMenuBtn.addEventListener("click",toggleMenu);
 closeMenuBtn.addEventListener("click",toggleMenu);
 inicio.addEventListener("click",function(){
     scrollTo(0,0);
-})
+});
+menuEnlaces.addEventListener("click",toggleMenu);
+
+   
